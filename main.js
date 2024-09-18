9b14f2b00ec463a421987b638c01bcbece6773f2
VitKall <ricardovitola@outlook.com>
2024-09-18 00:30:13 -0500

    Versión con slider

--- main.js
+++ main.js
@@ -2,35 +2,27 @@ let caracter = document.getElementById('cantidad');
 let boton = document.getElementById('generar');
 let contrasena = document.getElementById('contrasena');
 
-const cadenaCaracteres = '^TE^IbBp3fY*@I14zCm2lst*Z_s0ññ(T)dDqYij#HJ%n@9Ñ2#p_CyKZx-Uthz)jPLcqWñ¡*fcqiyuH8!#P6rAk8&loañ7pM0WaMVkD16g4ñL$!-Ñ8wBnuK¡1lV-7ÑFoQ5Q33NñOrcRbfGk7s5a9z%j@yt)vd$Nu%4Sm9$h_SX¡&2F!r0ewÑ6^vGbminAxxo&wXvOEUeÑJe5¡d';
+const cadenaCaracteres = '7|/?Q9i:I[#ÑcfE|qOlhcmT;op-&n&.K>boOñpxRn_=j}g>B@yñMC)<Yms1HvG1qG{4D%Wy3]*FLQd$,5IKzw5]aV;dJ8i:@a+%T4,N[H6)k?S/V#SM6UFYJ2{w=X8!E$+<Z}h9.0v(rDXZb!-g20*A3LPRxs_jNftztCekPÑ^r(uWu7AUeBl^';
 
 function generar() {
     let entradaUsuario = parseInt(cantidad.value);
     let password = ' ';
 
-    if (entradaUsuario < 13) {
-        asignarTextoElemento('h1', `Por favor, ingrese`);
-        asignarTextoElemento('span', `un número mayor a 12`);
+    for (i = 0; i < entradaUsuario; i++) {
+        let aleatorio = cadenaCaracteres[Math.floor(Math.random() * cadenaCaracteres.length)];
+        password += aleatorio;
+        contrasena.value = password;
+
+        asignarTextoElemento('h1', `Contraseña`);
+        asignarTextoElemento('span', `generada con éxito`);
         document.getElementById('borrar').removeAttribute('disabled');
-    }
-
-    else {
-        for (i = 0; i < entradaUsuario; i++) {
-            let aleatorio = cadenaCaracteres[Math.floor(Math.random() * cadenaCaracteres.length)];
-            password += aleatorio;
-            contrasena.value = password;
-
-            asignarTextoElemento('h1', `Contraseña`);
-            asignarTextoElemento('span', `generada con éxito`);
-            document.getElementById('borrar').removeAttribute('disabled');
-        }
     }
 }
 
 // *** FUNCIONES PARA REINICIAR *** \\
 
 function limpiarCaja() {
-    document.querySelector('#cantidad').value = '';
+    document.querySelector('#cantidad').value = '14';
 }
 
 function condicionesIniciales() {
